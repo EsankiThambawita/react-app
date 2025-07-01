08b5e13d0523eeb4c7ae14339bdb7583f322e0e1
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-07-01 20:47:12 +0530

    learned to display a list and use onClick

--- src/Message.tsx
+++ src/Message.tsx
@@ -3,7 +3,7 @@ function Message () {
     //This is not html. This is JSX(JavaScript XML)
     //This piece of code <h1>Hello World!</h1> gets converted to js
     
-    const name = 'Esanki';
+    const name = '';
     if(name)
         return <h1>Hello {name}</h1>;
     return <h1>Hello World!</h1>;
